7ad59bcd228e344a8868446549cdf51ba211d874
Raaz Khoshnood <raazkhoshnood@gmail.com>
2024-10-19 22:59:00 -0500

    refactor: renamed project variables

--- src/test/java/com/compassCameraControl/CompassCameraControlPluginTest.java
+++ src/test/java/com/compassCameraControl/CompassCameraControlPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.compassCameraControl;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class CompassCameraControlPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(CompassCameraControlPlugin.class);
 		RuneLite.main(args);
 	}
 }
